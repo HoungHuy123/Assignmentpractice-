705754e2e950538a2566a40d75b8005d18fbb6e1
HoungHuy123 <43284865+HoungHuy123@users.noreply.github.com>
2019-02-05 17:23:16 -0800

    Add files via upload

---
+++ Reverse.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+
+
+
+int main() {
+     
+    int num;
+    int *arr;
+
+    cout << "Enter the array number: ";
+    cin >> num;
+    cout << "\nEnter the " << num << "numbers ";
+    cin >> arr[num];
+
+    
+
+    return 0;
+}
